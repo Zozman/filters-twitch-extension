45b8a80e6dcf125277c0a572b1d91bf395cda392
Zac Lovoy <zwlovoy@gmail.com>
2024-10-06 22:34:54 -0700

    Updated localization

--- src/config/extension-config/index.ts
+++ src/config/extension-config/index.ts
@@ -17,27 +17,6 @@ import { TWITCH_THEMES, TwitchExtensionContext } from '../../types/twitch';
 import { SlCarousel, SlSlideChangeEvent } from '@shoelace-style/shoelace';
 
 /**
- * The steps for the demo carosall
- */
-const demoSteps = [
-    {
-        'text': msg('Click the button to open the filter menu and select a filter to apply it.'),
-        'lightImageUrl': demoStep1Light,
-        'darkImageUrl': demoStep1Dark
-    },
-    {
-        'text': msg('Enable the Filter Slider to adjust what side of the screen is filtered, and feel free to move around the menu by dragging the button.'),
-        'lightImageUrl': demoStep2Light,
-        'darkImageUrl': demoStep2Dark
-    },
-    {
-        'text': 'Use the Customize menu to adjust your filter or make one from scratch.',
-        'lightImageUrl': demoStep3Light,
-        'darkImageUrl': demoStep3Dark
-    }
-];
-
-/**
  * Configuration page for the extension
  */
 @customElement('extension-config')
@@ -88,30 +67,61 @@ export default class ExtensionConfig extends ExtensionBase {
         this.currentDemoStep = event.detail.index;
     }
 
+    /**
+     * Function to get the demo steps.  Done like this so locale is taken into account.
+     * @returns The demo steps
+     */
+    private getDemoSteps() {
+        return [
+            {
+                'text': msg('Click the button to open the filter menu and select a filter to apply it.'),
+                'lightImageUrl': demoStep1Light,
+                'darkImageUrl': demoStep1Dark
+            },
+            {
+                'text': msg('Enable the Filter Slider to adjust what side of the screen is filtered, and feel free to move around the menu by dragging the button.'),
+                'lightImageUrl': demoStep2Light,
+                'darkImageUrl': demoStep2Dark
+            },
+            {
+                'text': msg('Use the Customize menu to adjust your filter or make one from scratch.'),
+                'lightImageUrl': demoStep3Light,
+                'darkImageUrl': demoStep3Dark
+            }
+        ];
+    }
+
     render() {
         return html`
-            <sl-card class="holderCard">
-                <div slot="header" class="holderCardHeader">${msg("No configuration required, so here's how to use Filters.")}</div>
-                <sl-carousel
-                    class="demoCarousel"
-                    pagination
-                    navigation
-                    mouse-dragging
-                    loop
-                    @sl-slide-change="${this.onSlideChange}">
-                        ${demoSteps.map((demoStep, index) => html`
-                            <sl-carousel-item>
-                                <img
-                                    alt="${msg(str`Demo step ${index + 1} using the ${this.theme} theme`)}"
-                                    src="${this.theme === TWITCH_THEMES.LIGHT ? demoStep.lightImageUrl : demoStep.darkImageUrl}"
-                                />
-                            </sl-carousel-item>
-                        `)}
-                </sl-carousel>
-                <div slot="footer" class="holderFooter">
-                    ${demoSteps[this.currentDemoStep].text}
-                </div>
-            </sl-card>
+            ${this.loadLocaleTask.render({
+                complete: () => {
+                    const demoSteps = this.getDemoSteps();
+                    return html`
+                        <sl-card class="holderCard">
+                            <div slot="header" class="holderCardHeader">${msg("No configuration required, so here's how to use Filters.")}</div>
+                            <sl-carousel
+                                class="demoCarousel"
+                                pagination
+                                navigation
+                                mouse-dragging
+                                loop
+                                @sl-slide-change="${this.onSlideChange}">
+                                    ${demoSteps.map((demoStep, index) => html`
+                                        <sl-carousel-item>
+                                            <img
+                                                alt="${msg(str`Demo step ${index + 1} using the ${this.theme} theme`)}"
+                                                src="${this.theme === TWITCH_THEMES.LIGHT ? demoStep.lightImageUrl : demoStep.darkImageUrl}"
+                                            />
+                                        </sl-carousel-item>
+                                    `)}
+                            </sl-carousel>
+                            <div slot="footer" class="holderFooter">
+                                ${demoSteps[this.currentDemoStep].text}
+                            </div>
+                        </sl-card>
+                    `
+                }
+            })}
         `;
     }
 }
